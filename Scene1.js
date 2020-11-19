828aa6545c2cc70e65dff025b94d90d1354dea03
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-11-19 14:47:45 -0500

    got some not so good looking rain working

--- Scene1.js
+++ Scene1.js
@@ -14,6 +14,14 @@ class Scene1 extends Phaser.Scene {
         //weather object created to get map
         var map = new Weather().mapName;
         console.log(map + "1");
+
+        //if weather == weather preload weather spritesheet
+
+        this.load.spritesheet("rain", "assets/spritesheets/rain.png", {
+            //half of the spritesheet's width and height
+            frameWidth: 266,
+            frameHeight: 250
+        });
     }
 
     create() {
@@ -25,5 +33,11 @@ class Scene1 extends Phaser.Scene {
             frameRate: 20,
             repeat: -1
         });
+        this.anims.create( {
+            key: "rain_anim",
+            frames: this.anims.generateFrameNumbers("rain"),
+            frameRate: 20,
+            repeat: -1
+        })        
     }
 }
--- Scene2.js
+++ Scene2.js
@@ -4,7 +4,15 @@ class Scene2 extends Phaser.Scene {
     }
 
     create() {
+        //rain sprite added to canvas
+        this.rain = this.add.sprite(0, 0, "rain");
+        this.rain.setOrigin(0, 0);
+        this.rain.setScale(3.2);
+        this.rain.play("rain_anim");
+
+        //player sprite added to canvas
         this.player = this.add.sprite(this.game.config.width / 2 - 50, this.game.config.height / 2, "player");
         this.player.play("player_anim");
+
     }
 }
