dbc6424ae697ff13a8aefe4c4f71925ad1c43221
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-11-22 22:32:26 -0500

    refactored so that only the needed assets are loaded and created

--- Scene1.js
+++ Scene1.js
@@ -1,12 +1,11 @@
+var map;
+
 class Scene1 extends Phaser.Scene {
     constructor() {
         super("bootGame");
     }
 
     preload() {
-        //clear map
-        this.load.image("clearMap", "assets/maps/clearMap.png");
-
         //player ship
         this.load.spritesheet("player", "assets/spritesheets/player.png", {
             //half of the spritesheet's width
@@ -15,71 +14,108 @@ class Scene1 extends Phaser.Scene {
         });
 
         //callback function to fetch map
-        getMap(function(map) {
-            console.log(map);
+        getMap(function(mapFetched) {
+            map = mapFetched
         });
+
         //if weather == weather preload weather spritesheet
+        switch(map) {
+        case "thunderstorm":
+            this.load.spritesheet("lightning", "assets/spritesheets/lightning.png", {
+                frameWidth:266,
+                frameHeight: 250
+            });
+            break;
+        case "drizzle":
+            this.load.spritesheet("rain", "assets/spritesheets/rain.png", {
+                //half of the spritesheet's width and height
+                frameWidth: 266,
+                frameHeight: 250
+            });
+            break;
+        case "rain":
+            this.load.spritesheet("rain", "assets/spritesheets/rain.png", {
+                //half of the spritesheet's width and height
+                frameWidth: 266,
+                frameHeight: 250
+            });
+            this.load.audio("rain_audio", ["assets/audio/rain.ogg"]);
+            break;
+        case "snow":
+            this.load.spritesheet("snow", "assets/spritesheets/snow.png", {
+                frameWidth:800,
+                frameHeight: 600
+            });
+            break;
+        case "clear":
+            this.load.image("clearMap", "assets/maps/clearMap.png");
+            break;
+        case "clouds":
+            this.load.spritesheet("cloudy", "assets/spritesheets/cloudy.png", {
+                frameWidth:800,
+                frameHeight: 600
+            });
+            break;
+        default: 
+            
+            break;
+        }
 
-        //rain
-        this.load.spritesheet("rain", "assets/spritesheets/rain.png", {
-            //half of the spritesheet's width and height
-            frameWidth: 266,
-            frameHeight: 250
-        });
-        //lightning
-        this.load.spritesheet("lightning", "assets/spritesheets/lightning.png", {
-            frameWidth:266,
-            frameHeight: 250
-        });
-        //snow
-        this.load.spritesheet("snow", "assets/spritesheets/snow.png", {
-            frameWidth:800,
-            frameHeight: 600
-        });
-        //clouds
-        this.load.spritesheet("cloudy", "assets/spritesheets/cloudy.png", {
-            frameWidth:800,
-            frameHeight: 600
-        });
-
-        //sound files
-        this.load.audio("rain_audio", ["assets/audio/rain.ogg"]);
+        //music 
         this.load.audio("music", ["assets/audio/star_fighters_theme.wav"]);
     }
 
     create() {
         this.scene.start("playGame");
 
+        switch(map) {
+            case "thunderstorm":
+                this.anims.create( {
+                    key: "lightning_anim",
+                    frames: this.anims.generateFrameNumbers("lightning"),
+                    frameRate: 15,
+                    repeat: -1
+                });
+                break;
+            case "drizzle":
+                
+                break;
+            case "rain":
+                this.anims.create( {
+                    key: "rain_anim",
+                    frames: this.anims.generateFrameNumbers("rain"),
+                    frameRate: 20,
+                    repeat: -1
+                });
+                break;
+            case "snow":
+                this.anims.create( {
+                    key: "snow_anim",
+                    frames: this.anims.generateFrameNumbers("snow"),
+                    frameRate: 5,
+                    repeat: -1
+                });
+                break;
+            case "clear":    
+                break;
+            case "clouds":
+                //framerate or spritesheet may need work 
+                this.anims.create( {
+                    key: "cloudy_anim",
+                    frames: this.anims.generateFrameNumbers("cloudy"),
+                    frameRate: 2,
+                    repeat: -1
+                });
+                break;
+            default: 
+                
+                break;
+        }
         this.anims.create( {
             key: "player_anim",
             frames: this.anims.generateFrameNumbers("player"),
             frameRate: 20,
             repeat: -1
-        });
-        this.anims.create( {
-            key: "rain_anim",
-            frames: this.anims.generateFrameNumbers("rain"),
-            frameRate: 20,
-            repeat: -1
-        });
-        this.anims.create( {
-            key: "lightning_anim",
-            frames: this.anims.generateFrameNumbers("lightning"),
-            frameRate: 15,
-            repeat: -1
-        });
-        this.anims.create( {
-            key: "snow_anim",
-            frames: this.anims.generateFrameNumbers("snow"),
-            frameRate: 5,
-            repeat: -1
-        });
-        //framerate or spritesheet may need work   
-        this.anims.create( {
-            key: "cloudy_anim",
-            frames: this.anims.generateFrameNumbers("cloudy"),
-            frameRate: 2,
-            repeat: -1
-        });       
+        });           
     }
 }
